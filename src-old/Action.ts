75c76f8ac205cf2a26e90c928e68c419b9719512
Homa Wong <homawong@gmail.com>
2017-04-08 22:13:25 -0700

    Remove old code

--- src-old/Action.ts
+++
@@ -1,6 +0,0 @@
-import { CommandBuilder } from './CommandBuilder'
-import { UI } from './CliBuilder'
-
-export interface Action<A, O> {
-  (args: A, options: O, builder: CommandBuilder, ui: UI): boolean | void
-}
--- src-old/index.ts
+++
@@ -1,8 +0,0 @@
-import { CliBuilder } from './CliBuilder'
-export * from './Argument'
-export * from './CliBuilder'
-export * from './CommandBuilder'
-export * from './HelpSectionBuilder'
-export * from './Option'
-
-export default new CliBuilder()
