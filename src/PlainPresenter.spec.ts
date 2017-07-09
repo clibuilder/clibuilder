7e568a5b482b9406f3b476cbe5b5d62b49efa37f
Homa Wong <homawong@gmail.com>
2017-07-08 17:58:50 -0700

    show default values (#39)

--- src/PlainPresenter.spec.ts
+++ src/PlainPresenter.spec.ts
@@ -16,3 +16,38 @@ test('should default to print info, warn, error messages', t => {
   t.is(display.errorLogs[0][0], 'e')
   t.is(display.infoLogs[0][0], 'i')
 })
+
+test('help message indicates default boolean option', t => {
+  const p = new PlainPresenter({ name: 'a' })
+  const display = new InMemoryDisplay()
+  p.display = display
+  p.showHelp({
+    name: 'a', options: {
+      boolean: { 'some-option': { description: 'some description', default: true } }
+    }
+  })
+  t.is(display.infoLogs[0][0], `
+Usage: a
+
+Options:
+  [--some-option]        some description (default true)
+`)
+})
+
+
+test('help message indicates default string option', t => {
+  const p = new PlainPresenter({ name: 'a' })
+  const display = new InMemoryDisplay()
+  p.display = display
+  p.showHelp({
+    name: 'a', options: {
+      string: { 'some-option': { description: 'some description', default: 'yes' } }
+    }
+  })
+  t.is(display.infoLogs[0][0], `
+Usage: a
+
+Options:
+  [--some-option]=value  some description (default 'yes')
+`)
+})
