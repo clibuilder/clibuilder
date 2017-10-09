676799494dcdb662339fc496ff99e9139de1d347
Homa Wong <homawong@gmail.com>
2017-10-09 16:25:18 -0700

    Also re-export EventEmitter2

--- src/test/DummyPlugin.ts
+++ src/test/DummyPlugin.ts
@@ -1,8 +1,23 @@
-import { CliRegistrar, Command } from '../index'
+import { CliRegistrar, Command, createTaskRunner, Task, ViewContext, EventEmitter2 } from '../index'
+
+class DummyTask extends Task {
+  run() {
+    this.emitter.emit('dummy')
+  }
+}
+
+function dummyViewBuilder(emitter: EventEmitter2, { ui }: ViewContext) {
+  emitter.on('dummy', () => {
+    ui.info('dummy!')
+  })
+}
 
 const DummyCommand = {
   name: 'dummy',
-  run() { return }
+  run() {
+    const runner = createTaskRunner(this, DummyTask, dummyViewBuilder)
+    runner.run()
+  }
 } as Command
 
 export function activate(cli: CliRegistrar) {
