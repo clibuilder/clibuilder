84993f6675da31fceed86c191241950c68320ea1
Homa Wong <homawong@gmail.com>
2017-10-10 19:34:42 -0700

    fix: will get the right cmd when there are sub commands (#66)

--- src/util.spec.ts
+++ src/util.spec.ts
@@ -1,6 +1,7 @@
 import test, { TestContext } from 'ava'
 
-import { getCommandAndAliasNames } from './util'
+import { getCommandAndAliasNames, getCommand, createCommand } from './util'
+import { InMemoryPresenterFactory } from './test-util/InMemoryPresenterFactory';
 
 test('should return empty array with no commands defined', t => {
   assertGettingNamesAndAlias(t, [], [])
@@ -29,3 +30,19 @@ test('should sort names in alphabetical order', t => {
 function assertGettingNamesAndAlias(t: TestContext, actual: { name: string, alias?: string[] }[], names: string[]) {
   t.deepEqual(getCommandAndAliasNames(actual), names)
 }
+
+test('run 1st level command', t => {
+  const cli = {
+    name: 'clibuilder',
+    commands: [{
+      name: 'cmd',
+      commands: [{
+        name: 'nested-cmd'
+      }]
+    }]
+  }
+  const cmd = createCommand(cli, { cwd: '.', parent: undefined, presenterFactory: new InMemoryPresenterFactory() })
+  const actual = getCommand(['clibuilder', 'cmd'], [cmd])
+  t.not(actual, undefined)
+  t.is(actual!.name, 'cmd')
+})
