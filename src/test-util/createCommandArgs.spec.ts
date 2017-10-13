f3b9a5f5d104fba701449ae2f9e31c413251e797
Homa Wong <homawong@gmail.com>
2017-10-13 11:48:15 -0700

    Revert createCommandArgs to take argv[] instead of ...argv
    While it is easier to use directly with ...argv, the actual usage call for argv[]

--- src/test-util/createCommandArgs.spec.ts
+++ src/test-util/createCommandArgs.spec.ts
@@ -14,31 +14,31 @@ test('empty argv', t => {
 })
 
 test(`one argv`, t => {
-  const args = createCommandArgs(argCommand, 'arg1')
+  const args = createCommandArgs(argCommand, ['arg1'])
 
   t.deepEqual(args, { _: ['arg1'] })
 })
 
 test(`boolean option using with '--'`, t => {
-  const args = createCommandArgs(booleanOptionsCommand, '--a')
+  const args = createCommandArgs(booleanOptionsCommand, ['--a'])
 
   t.deepEqual(args, { a: true, b: false, _: [] })
 })
 
 test('string option with =', t => {
-  const args = createCommandArgs(stringOptionCommand, '--a=abc')
+  const args = createCommandArgs(stringOptionCommand, ['--a=abc'])
 
   t.deepEqual(args, { a: 'abc', _: [] })
 })
 
 test.failing('string option with space', t => {
-  const args = createCommandArgs(stringOptionCommand, '-a 3')
+  const args = createCommandArgs(stringOptionCommand, ['-a 3'])
 
   t.deepEqual(args, { a: '3', _: [] })
 })
 
 test('string option without space', t => {
-  const args = createCommandArgs(stringOptionCommand, '-a5')
+  const args = createCommandArgs(stringOptionCommand, ['-a5'])
 
   t.deepEqual(args, { a: '5', _: [] })
 })
