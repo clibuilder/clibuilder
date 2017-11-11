1f337c2fdf738d183a1c9caf16ee986faa30e01f
unional <homawong@gmail.com>
2017-11-11 15:15:07 -0800

    Test for custom keywords

--- src/PluginCli.spec.ts
+++ src/PluginCli.spec.ts
@@ -9,3 +9,12 @@ test('use "{name}-plugin" as keyword to look for plugins', t => {
   }, { cwd: 'fixtures/one-plugin' })
   t.is(cli.commands.length, 2)
 })
+
+test('use custom keyword to look for plugins', t => {
+  const cli = new PluginCli({
+    name: 'clibuilder',
+    version: '1.0.0',
+    keyword: 'x-file'
+  }, { cwd: 'fixtures/alt-keyword-plugin' })
+  t.is(cli.commands.length, 2)
+})
