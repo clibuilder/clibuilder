7289934e7d21c3097db6e88338618a9a8e2be1b1
unional <homawong@gmail.com>
2017-11-11 15:21:08 -0800

    fix test

--- src/PluginCli.spec.ts
+++ src/PluginCli.spec.ts
@@ -7,6 +7,8 @@ test('use "{name}-plugin" as keyword to look for plugins', t => {
     name: 'clibuilder',
     version: '1.0.0'
   }, { cwd: 'fixtures/one-plugin' })
+  // there is an "global" `clibuilder-plugin-dummy` inside this project.
+  // That's why there are two commands instead of one.
   t.is(cli.commands.length, 2)
 })
 
@@ -16,5 +18,5 @@ test('use custom keyword to look for plugins', t => {
     version: '1.0.0',
     keyword: 'x-file'
   }, { cwd: 'fixtures/alt-keyword-plugin' })
-  t.is(cli.commands.length, 2)
+  t.is(cli.commands.length, 1)
 })
