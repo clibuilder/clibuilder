afd7c53e2f0c0f65c4aed0ee197d8a7d1eeeb354
unional <homawong@gmail.com>
2017-11-19 03:29:19 -0800

    BREAKING: Remove Task. It is moved to fsa-emitter.

--- src/loadPlugins.spec.ts
+++ src/loadPlugins.spec.ts
@@ -2,15 +2,20 @@ import test from 'ava'
 
 import { loadPlugins } from './loadPlugins'
 
+test('empty folder will still load global plugins', t => {
+  const plugins = loadPlugins('clibuilder-plugin', { cwd: 'fixtures/no-plugin' })
+  t.is(plugins.length, 1)
+})
+
+test(`loads no plugin when plugin's activate is not a function`, t => {
+  const plugins = loadPlugins('clibuilder-plugin', { cwd: 'fixtures/bad-plugin' })
+  t.is(plugins.length, 1)
+})
+
 test('loads one plugin in one-plugin folder', t => {
   const plugins = loadPlugins('clibuilder-plugin', { cwd: 'fixtures/one-plugin' })
   t.is(plugins.length, 2)
   t.is(plugins[0].name, 'x')
-})
-
-test('empty folder will still load global plugins', t => {
-  const plugins = loadPlugins('clibuilder-plugin', { cwd: 'fixtures/no-plugin' })
-  t.is(plugins.length, 1)
 })
 
 test('local plugin will prevent global plugin to load', t => {
--- src/test-util/stringOptionCommand.spec.ts
+++ src/test-util/stringOptionCommand.spec.ts
@@ -1,27 +1,27 @@
 import { test } from 'ava'
-import { setupCommandTest, argCommand, stringOptionCommand } from '../index'
+import { setupCliCommandTest, argCommand, stringOptionCommand } from '../index'
 
 
 test('string option with space', t => {
-  const { args } = setupCommandTest(stringOptionCommand, ['-a 3'])
+  const { args } = setupCliCommandTest(stringOptionCommand, ['-a 3'])
 
   t.deepEqual(args, { a: '3', _: [], _defaults: [] })
 })
 
 test('string option with =', t => {
-  const { args } = setupCommandTest(stringOptionCommand, ['-a=abc'])
+  const { args } = setupCliCommandTest(stringOptionCommand, ['-a=abc'])
 
   t.deepEqual(args, { a: 'abc', _: [], _defaults: [] })
 })
 
 test('string option without space', t => {
-  const { args } = setupCommandTest(stringOptionCommand, ['-a5'])
+  const { args } = setupCliCommandTest(stringOptionCommand, ['-a5'])
 
   t.deepEqual(args, { a: '5', _: [], _defaults: [] })
 })
 
 test('log option', t => {
-  const { cmd, args, argv, ui } = setupCommandTest(stringOptionCommand, ['-a 3'])
+  const { cmd, args, argv, ui } = setupCliCommandTest(stringOptionCommand, ['-a 3'])
 
   cmd.run(args, argv)
 
@@ -29,7 +29,7 @@ test('log option', t => {
 })
 
 test('argument command', t => {
-  const { cmd, args, argv, ui } = setupCommandTest(argCommand, ['abc'])
+  const { cmd, args, argv, ui } = setupCliCommandTest(argCommand, ['abc'])
 
   t.deepEqual(args, { _: [], _defaults: [], 'some-arg': 'abc' })
 
