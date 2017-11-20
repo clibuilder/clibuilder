fae08f80f09a1ad2300468ea1ccb9835c01f0800
unional <homawong@gmail.com>
2017-11-19 19:14:07 -0800

    fest: InMemoryPresenterFactory reuse presenters.
    And exposing them so they can be easily used in test.
    
    Rename fixture plugin to make it looks cleaner.

--- src/loadPlugins.spec.ts
+++ src/loadPlugins.spec.ts
@@ -15,11 +15,11 @@ test(`loads no plugin when plugin's activate is not a function`, t => {
 test('loads one plugin in one-plugin folder', t => {
   const plugins = loadPlugins('clibuilder-plugin', { cwd: 'fixtures/one-plugin' })
   t.is(plugins.length, 2)
-  t.is(plugins[0].name, 'x')
+  t.is(plugins[0].name, 'one')
 })
 
 test('local plugin will prevent global plugin to load', t => {
   const plugins = loadPlugins('clibuilder-plugin', { cwd: 'fixtures/local-plugin' })
   t.is(plugins.length, 1)
-  t.is(plugins[0].name, 'x')
+  t.is(plugins[0].name, 'dummy')
 })
