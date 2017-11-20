c45c8e6ab66bf32a737b81745d8fefeed022ed10
Homa Wong <homawong@gmail.com>
2017-11-19 22:51:22 -0800

    perf: makes loadPlugins() async (#107)
    * perf: makes loadPlugins() async
    
    closes #54
    
    * Clean up readDirSafe
    
    * test: Add test to ensure when parse the commands are there.
    
    * test: fix test

--- src/PluginCli.spec.ts
+++ src/PluginCli.spec.ts
@@ -1,22 +1,40 @@
 import { test } from 'ava'
 
-import { PluginCli } from './index'
+import { PluginCli, InMemoryPresenterFactory, createCliArgv } from './index'
 
-test('use "{name}-plugin" as keyword to look for plugins', t => {
-  const cli = new PluginCli({
+class TestPluginCli extends PluginCli {
+  ready = this.loadingPlugins
+}
+
+test('use "{name}-plugin" as keyword to look for plugins', async t => {
+  const cli = new TestPluginCli({
     name: 'clibuilder',
     version: '1.0.0'
   }, { cwd: 'fixtures/one-plugin' })
+  await cli.ready
   // there is an "global" `clibuilder-plugin-dummy` inside this project.
   // That's why there are two commands instead of one.
   t.is(cli.commands.length, 2)
 })
 
-test('use custom keyword to look for plugins', t => {
-  const cli = new PluginCli({
+test('use custom keyword to look for plugins', async t => {
+  const cli = new TestPluginCli({
     name: 'clibuilder',
     version: '1.0.0',
     keyword: 'x-file'
   }, { cwd: 'fixtures/alt-keyword-plugin' })
+  await cli.ready
   t.is(cli.commands.length, 1)
+})
+
+test('command is loaded when parse', async t => {
+  const presenterFactory = new InMemoryPresenterFactory()
+  const cli = new PluginCli({
+    name: 'clibuilder',
+    version: '1.0.0'
+  }, { cwd: 'fixtures/one-plugin', presenterFactory })
+
+  await cli.parse(createCliArgv('clibuilder', 'one', 'echo'))
+  const presenter = presenterFactory.commandPresenter
+  t.is(presenter.display.infoLogs[0][0], 'echo')
 })
--- src/findPlugins.spec.ts
+++ src/findPlugins.spec.ts
@@ -2,14 +2,14 @@ import test from 'ava'
 
 import { findPlugins } from './findPlugins'
 
-test('find no plugin in empty folder', t => {
-  t.deepEqual(findPlugins('clibuilder-plugin', 'fixtures/no-plugin'), [])
+test('find no plugin in empty folder', async t => {
+  t.deepEqual(await findPlugins('clibuilder-plugin', 'fixtures/no-plugin'), [])
 })
 
-test('find one plugin on one-plugin folder', t => {
-  t.deepEqual(findPlugins('clibuilder-plugin', 'fixtures/one-plugin'), ['cli-plugin-one'])
+test('find one plugin on one-plugin folder', async t => {
+  t.deepEqual(await findPlugins('clibuilder-plugin', 'fixtures/one-plugin'), ['cli-plugin-one'])
 })
 
-test('find one plugin in scoped-plugin folder', t => {
-  t.deepEqual(findPlugins('clibuilder-plugin', 'fixtures/scoped-plugin'), ['@cli/y'])
+test('find one plugin in scoped-plugin folder', async t => {
+  t.deepEqual(await findPlugins('clibuilder-plugin', 'fixtures/scoped-plugin'), ['@cli/y'])
 })
