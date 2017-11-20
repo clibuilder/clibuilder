9564e59fda78cc07f73c845fcf27646d7b6c76bb
unional <homawong@gmail.com>
2017-11-19 17:59:18 -0800

    chore: improve coverage

--- src/PluginCli.ts
+++ src/PluginCli.ts
@@ -13,6 +13,8 @@ export interface PluginCliContext extends CliContext {
 export class PluginCli<Context extends { [i: string]: any } = {}> extends Cli {
   constructor(options: PluginCliOption, context: Partial<PluginCliContext> & Context = {} as any) {
     let { name, version, keyword = `${options.name}-plugin` } = options
+
+    // istanbul ignore next
     const cwd = context.cwd || process.cwd()
 
     const commands = loadPlugins(keyword, { cwd })
