741710a08c137b2f0c67935f1ef13d284aa7e762
unional <homawong@gmail.com>
2018-01-03 22:42:59 -0800

    chore: update deps

--- src/overrideArgs.ts
+++ src/overrideArgs.ts
@@ -3,7 +3,6 @@ import _ = require('lodash')
 import { CliArgs } from './interfaces';
 import { toArgsWithoutDefaults } from './toArgsWithoutDefaults';
 
-
 export function overrideArgs(args: CliArgs, config?: any): { [k: string]: any } {
   const bare = toArgsWithoutDefaults(args)
   const result = _.merge({}, args, config, bare)
