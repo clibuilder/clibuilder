ae79d6e975551e0c0da9061b1d63c7694545b2cb
Homa Wong <homawong@gmail.com>
2018-12-04 23:32:49 -0800

    fix: setupCliCommandTest will always get InMemoryPresenter as ui. (#180)

--- src/test-util/setup.spec.ts
+++ src/test-util/setup.spec.ts
@@ -1,4 +1,6 @@
 import { setupCliCommandTest, CliCommand } from '../'
+import { PlainPresenter } from '../PlainPresenter';
+import { InMemoryPresenter } from './InMemoryPresenter';
 
 test('specifying Config gets completion support', () => {
   const cmd: CliCommand<{ foo: string }, { boo: string }> = {
@@ -9,4 +11,27 @@ test('specifying Config gets completion support', () => {
   // and inside `config` it does not get `foo`.
   // Explicit generic does not have this problem.
   setupCliCommandTest<{ foo: string }, { boo: string }>(cmd, [], { boo: 'boo', config: { foo: 'foo' } })
+})
+
+test('command with overriden ui still get memory presenter for testing', () => {
+  const cmd: CliCommand<{ foo: string }, { boo: string }> = {
+    name: 'override-ui',
+    ui: new PlainPresenter()
+  }
+  const { ui } = setupCliCommandTest<{ foo: string }, { boo: string }>(cmd, [])
+
+  expect(ui).toBeInstanceOf(InMemoryPresenter)
+})
+
+test('sub-command with overriden ui still get memory presenter for testing', () => {
+  const cmd: CliCommand<{ foo: string }, { boo: string }> = {
+    name: 'override-ui-in-sub',
+    commands: [{
+      name: 'override-ui-sub',
+      ui: new PlainPresenter()
+    }]
+  }
+  const { ui } = setupCliCommandTest<{ foo: string }, { boo: string }>(cmd, [])
+
+  expect(ui).toBeInstanceOf(InMemoryPresenter)
 })
