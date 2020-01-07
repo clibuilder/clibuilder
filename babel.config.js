bcff249a64246545ce37716274fbfc600b3668f6
Homa Wong <homawong@gmail.com>
2020-01-06 18:11:25 -0800

    build: reset back to use tsc
    target es2019

--- babel.config.js
+++ babel.config.js
@@ -13,10 +13,7 @@ module.exports = {
     }],
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
-    '@babel/plugin-proposal-numeric-separator'
   ],
 };
