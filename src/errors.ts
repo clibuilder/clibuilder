a00a0ed812b2d8dd4e2f72d56aed10bf2dbf21ba
Homa Wong <homawong@gmail.com>
2020-01-06 18:16:38 -0800

    chore: point typings to lib
    libm may change again in the future to build with babel

--- src/errors.ts
+++ src/errors.ts
@@ -1,7 +1,6 @@
 import { IsoError } from 'iso-error'
 
 export class NotNumberOption extends IsoError {
-  // istanbul ignore next
   constructor(public name: string) {
     super(`Option '${name}' only accepts number`)
     Object.setPrototypeOf(this, NotNumberOption.prototype)
@@ -9,7 +8,6 @@ export class NotNumberOption extends IsoError {
 }
 
 export class MissingArguments extends IsoError {
-  // istanbul ignore next
   constructor(public expected: number, public actual: number) {
     super((expected <= 1 ? `Missing Argument.` : `Missing Arguments.`) + ` Expecting ${expected} but received ${actual}.`)
     Object.setPrototypeOf(this, MissingArguments.prototype)
@@ -17,7 +15,6 @@ export class MissingArguments extends IsoError {
 }
 
 export class TooManyArguments extends IsoError {
-  // istanbul ignore next
   constructor(public expected: number, public actual: number) {
     super(`Too Many Arguments. Expecting ${expected} but received ${actual}.`)
     Object.setPrototypeOf(this, TooManyArguments.prototype)
