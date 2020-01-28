54392ba30cb51be86407db5911e234673a4c1f16
Homa Wong <homawong@gmail.com>
2020-01-28 13:47:48 -0800

    feat: support js and rc config

--- src/cli/loadConfig.ts
+++ src/cli/loadConfig.ts
@@ -4,21 +4,42 @@ import path from 'path'
 
 const win = process.platform === 'win32'
 
-export function loadConfig(configFileName: string, { cwd, home }: { cwd?: string, home?: string } = {} as any) {
-  cwd = cwd || process.cwd()
-  home = home || (win ?
-    process.env.USERPROFILE :
-    process.env.HOME)
+export namespace loadConfig {
+  export type Options = {
+    cwd: string,
+    home?: string
+  }
+}
 
-  let filePath = findUp.sync(configFileName, { cwd })
-  if (!filePath && home)
-    filePath = path.join(home, configFileName)
+export function loadConfig(configName: string, { cwd, home }: loadConfig.Options) {
+  const filePath = discoverConfigFile(configName, {
+    cwd,
+    home: home || (win ? process.env.USERPROFILE : process.env.HOME)
+  })
   return filePath ? readConfig(filePath) : undefined
+}
+
+function discoverConfigFile(configName: string, options: loadConfig.Options) {
+  return findConfigFile(`${configName}.json`, options) ||
+    findConfigFile(`${configName}.js`, options) ||
+    findConfigFile(`${configName}rc`, options)
+}
+
+function findConfigFile(configFileName: string, { cwd, home }: loadConfig.Options) {
+  let filePath = findUp.sync(configFileName, { cwd })
+  if (!filePath && home) {
+    filePath = path.join(home, configFileName)
+    return fs.existsSync(filePath) ? filePath : undefined
+  }
+
+  return filePath
 }
 
 function readConfig(filePath: string) {
   try {
-    return JSON.parse(fs.readFileSync(filePath, 'utf8'))
+    return path.extname(filePath) === '.json' ?
+      JSON.parse(fs.readFileSync(filePath, 'utf-8')) :
+      require(filePath)
   }
   catch (err) {
     if (err.code === 'ENOENT')
