88358bce0bff1e29463c9053f0157f6870d11007
Homa Wong <homawong@gmail.com>
2020-02-29 15:33:45 -0800

    chore: improve coverage

--- src/test-util/contextCommand.ts
+++ src/test-util/contextCommand.ts
@@ -3,7 +3,10 @@ import { createCommand } from '../cli'
 export const contextCommand = createCommand({
   name: 'context',
   description: 'command returning itself (which has context merged)',
-  context: { foo() { return 'some string' } },
+  context: {
+    // istanbul ignore next
+    foo() { return 'some string' }
+  },
   run() {
     return this
   }
