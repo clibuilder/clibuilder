d31a3cabd602ad551da2052a85eaf857e3545f32
Homa Wong <homawong@gmail.com>
2021-02-25 02:27:47 -0800

    fix: clean up export types

--- src/command.ts
+++ src/command.ts
@@ -2,11 +2,11 @@ import * as z from 'zod'
 import type { cli } from './cli'
 
 export function command<
-  Config extends Record<string, any> = Record<string, any>,
+  ConfigType extends z.ZodTypeAny = z.ZodTypeAny,
   AName extends string = string,
   A extends cli.Command.Argument<AName>[] = cli.Command.Argument<AName>[],
   O extends cli.Command.Options = cli.Command.Options
->(cmd: cli.Command<Config, A, O>) {
+>(cmd: cli.Command<ConfigType, A, O>) {
   return cmd
 }
 
