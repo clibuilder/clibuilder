6f6df9281642d314085de37ef58e25848f2fc737
Homa Wong <homawong@gmail.com>
2021-03-05 00:55:36 -0800

    fix: clean up ui

--- src/ui.spec.ts
+++ src/ui.spec.ts
@@ -4,41 +4,32 @@ import { command } from './command'
 import { getLogMessage } from './test-utils'
 import { ui } from './ui'
 
-describe('showHelp()', () => {
-  test('options with single string alias', () => {
+describe('showVersion', () => {
+  test('undefined version shows "not versioned"', () => {
     const { ui, reporter } = testUI()
-    ui.showHelp('cli', command({
-      name: 'cmd',
-      options: {
-        long: { description: 'description', alias: ['l'] }
-      },
-      run() { }
-    }))
-    expect(getLogMessage(reporter)).toEqual(`
-Usage: cli cmd [options]
-
-Options:
-  [-l|--long]            description
-`)
+    ui.showVersion()
+    expect(getLogMessage(reporter)).toEqual('not versioned')
   })
-  test('options with hidden alias', () => {
+  test('empty version shows "not versioned"', () => {
+    const { ui, reporter } = testUI()
+    ui.showVersion('')
+    expect(getLogMessage(reporter)).toEqual('not versioned')
+  })
+})
+
+describe('showHelp()', () => {
+  test('command with alias', () => {
     const { ui, reporter } = testUI()
     ui.showHelp('cli', command({
-      name: 'cmd',
-      options: {
-        long: {
-          type: z.optional(z.string()),
-          description: 'description',
-          alias: [{ alias: 'l', hidden: true }]
-        }
-      },
+      name: 'search-package',
+      alias: ['sp'],
       run() { }
     }))
     expect(getLogMessage(reporter)).toEqual(`
-Usage: cli cmd [options]
+Usage: cli search-package
 
-Options:
-  [--long]               description
+Alias:
+  sp
 `)
   })
   test('with sub-commands', () => {
@@ -58,6 +49,222 @@ Commands:
 
 cmd <command> -h         Get help for <command>
 `)
+  })
+  test('sub command with alias', () => {
+    const { ui, reporter } = testUI()
+    ui.showHelp('cli', command({
+      name: 'repo',
+      commands: [{
+        name: 'search-package',
+        alias: ['sp'],
+        run() { }
+      }]
+    }))
+    expect(getLogMessage(reporter)).toEqual(`
+Usage: cli repo <command>
+
+Commands:
+  search-package (sp)
+
+repo <command> -h        Get help for <command>
+`)
+  })
+  test('required argument', () => {
+    const { ui, reporter } = testUI()
+    ui.showHelp('cli', command({
+      name: 'cmd',
+      arguments: [{ name: 'x', description: '' }],
+      run() { }
+    }))
+    expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <arguments>
+
+Arguments:
+  <x>
+`)
+  })
+  test('optional argument', () => {
+    const { ui, reporter } = testUI()
+    ui.showHelp('cli', command({
+      name: 'cmd',
+      arguments: [{ name: 'x', description: '', type: z.optional(z.string()) }],
+      run() { }
+    }))
+    expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd [arguments]
+
+Arguments:
+  [x]
+`)
+  })
+  describe('options', () => {
+    test('default (optional boolean)', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc' }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd [options]
+
+Options:
+  [--abc]                desc
+`)
+    })
+    test('required string', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.string() }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>=string         desc
+`)
+    })
+    test('required number', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.number() }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>=number         desc
+`)
+    })
+    test('required boolean', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.boolean() }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>                desc
+`)
+    })
+    test('required string array', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.array(z.string()) }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>=string...      desc
+`)
+    })
+    test('required number array', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.array(z.number()) }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>=number...      desc
+`)
+    })
+    test('required boolean array', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.array(z.boolean()) }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>=boolean...     desc
+`)
+    })
+    test('with default', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          'abc': { description: 'desc', type: z.string(), default: 'miku' }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd <options>
+
+Options:
+  <--abc>=string         desc (default 'miku')
+`)
+    })
+    test('options with single string alias', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          long: { description: 'description', alias: ['l'] }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd [options]
+
+Options:
+  [-l|--long]            description
+`)
+    })
+    test('options with hidden alias', () => {
+      const { ui, reporter } = testUI()
+      ui.showHelp('cli', command({
+        name: 'cmd',
+        options: {
+          long: {
+            type: z.optional(z.string()),
+            description: 'description',
+            alias: [
+              { alias: 'l', hidden: true },
+              { alias: 'h', hidden: false },
+            ]
+          }
+        },
+        run() { }
+      }))
+      expect(getLogMessage(reporter)).toEqual(`
+Usage: cli cmd [options]
+
+Options:
+  [-h|--long]=string     description
+`)
+    })
   })
 })
 
