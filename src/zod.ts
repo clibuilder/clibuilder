fc902c3915b442f54622f8ba1af7313364a09958
Homa Wong <homawong@gmail.com>
2021-03-26 14:23:29 -0700

    fix: replace instanceof check
    instanceof check does not work in plugin environment.
    
    Because in NodeJS,
    the plugin can be using its own copy of the zod library,
    under its own node_modules.
    
    This cause all instanceof check to fail.
    
    Need to rely of other mechanism to detect the type.

--- src/zod.ts
+++ src/zod.ts
@@ -1,1 +1,26 @@
-export * as z from 'zod'
+import * as z from 'zod'
+export { z }
+
+export function isZodArray(type: z.ZodAny | undefined): type is z.ZodArray<any> {
+  return type && (type as any).element
+}
+
+export function isZodOptional(type: z.ZodAny | undefined): type is z.ZodOptional<any> {
+  return !!type && type.isOptional()
+}
+
+export function isZodBoolean(type: z.ZodAny | undefined): type is z.ZodBoolean {
+  return isZodType(type, 'ZodBoolean')
+}
+
+export function isZodNumber(type: z.ZodAny | undefined): type is z.ZodBoolean {
+  return isZodType(type, 'ZodNumber')
+}
+
+export function isZodString(type: z.ZodAny | undefined): type is z.ZodString {
+  return isZodType(type, 'ZodString')
+}
+
+function isZodType(type: z.ZodAny | undefined, name: string) {
+  return Object.getPrototypeOf(type).constructor.name === name
+}
