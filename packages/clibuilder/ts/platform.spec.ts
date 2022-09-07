b563fce24c071cc286c97faecdbad35855b9d0e7
Homa Wong <homawong@gmail.com>
2022-09-07 00:56:46 -0700

    chore: add platform.js

---
+++ packages/clibuilder/ts/platform.spec.ts
@@ -0,0 +1,31 @@
+import path from 'path'
+import { ctx, findPackageJson, getHomePath, getPackageJson } from './platform.js'
+import { pathEqual } from 'path-equal'
+import { getFixturePath } from './test-utils/getFixturePath.js'
+describe('findPackageJson()', () => {
+  it('looks up to get the nearest package.json', () => {
+    const a = findPackageJson(path.resolve('.'))!
+    expect(pathEqual(a, path.resolve('./package.json'))).toBeTruthy()
+  })
+})
+
+describe('getPackageJson()', () => {
+  it('reads package.json', () => {
+    const jsonPath = findPackageJson(getFixturePath('cli-with-one-plugin'))!
+    const pjson = getPackageJson(jsonPath)
+    expect(pjson.type).toEqual('commonjs')
+  })
+})
+
+describe('getHomePath()', () => {
+  it('getS USERPROFILE for Windows', () => {
+    ctx.platform = 'win32'
+    ctx.USERPROFILE = 'C:/Users/miku'
+    expect(getHomePath()).toEqual('C:/Users/miku')
+  })
+  it('gets HOME for Non Windows', () => {
+    ctx.platform = 'linux'
+    ctx.HOME = '/usr/miku'
+    expect(getHomePath()).toEqual('/usr/miku')
+  })
+})
