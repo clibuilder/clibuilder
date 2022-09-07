eab75d39233f3de2844a3fb49eee251db4dc62f7
Homa Wong <homawong@gmail.com>
2022-09-07 00:56:46 -0700

    chore: add platform.js

--- packages/clibuilder/ts/loadAppInfo.ts
+++ packages/clibuilder/ts/loadAppInfo.ts
@@ -1,17 +1,15 @@
-import fs from 'fs'
-import { findUpSync } from 'find-up'
 import { dirname } from 'path'
+import { findPackageJson, getPackageJson } from './platform.js'
 import { createUI } from './ui.js'
 
 export function loadAppInfo(ui: createUI.UI, appPkgPath: string) {
   ui.debug(`finding package.json starting from '${appPkgPath}'...`)
-  const pjsonPath = findUpSync('package.json', { cwd: appPkgPath })
+  const pjsonPath = findPackageJson(appPkgPath)
   // istanbul ignore next
   if (pjsonPath) {
     ui.debug(`found package.json at '${pjsonPath}'`)
 
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const pjson = JSON.parse(fs.readFileSync(pjsonPath, 'utf-8'))
+    const pjson = getPackageJson(pjsonPath)
     return {
       name: pjson.name,
       version: pjson.version,
