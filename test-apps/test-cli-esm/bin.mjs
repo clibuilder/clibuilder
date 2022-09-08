679402a9f72bb61ea5976acff3406f2febde65ad
Homa Wong <homawong@gmail.com>
2022-09-07 23:58:09 -0700

    chore: add older fixtures
    May add more tests later on

---
+++ test-apps/test-cli-esm/bin.mjs
@@ -0,0 +1,25 @@
+import { cli } from 'clibuilder'
+
+const app = cli({
+  name: 'test-cli',
+  version: '1.0.0'
+}).default({
+  run() {
+    this.ui.error('some error message');
+    this.ui.info('some info message');
+  }
+}).command({
+  name: 'echo',
+  arguments: [{ name: 'value', description: 'value to echo back' }],
+  run({ value }) {
+    this.ui.info(`echoing: ${value}`);
+  }
+}).command({
+  name: 'config-value',
+  arguments: [{ name: 'name', description: 'config property name' }],
+  run({ name }) {
+    this.ui.info(`${name}: ${this.config[name]}`);
+  }
+})
+
+app.parse(process.argv)
