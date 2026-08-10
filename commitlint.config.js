8cad450f3298ecccd04b593f05adfe341da6a1fb
unional <homawong@gmail.com>
2026-08-09 18:25:47 -0700

    chore(commitlint): relax commit message casing and length
    Allow project commit messages to use the repository's existing capitalization and longer explanatory headers and bodies.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+export default {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"subject-case": [0],
+		"header-case": [0],
+		"header-max-length": [0],
+		"body-max-line-length": [0],
+	},
+};
